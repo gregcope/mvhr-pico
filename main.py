b9d61489f3b4d3d854a8a520689f419c05b39226
Greg Cope <gregcope@gmail.com>
2026-09-21 17:08:47 +0100

    Update test 1.4.7

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ try:
 except ImportError:
     from umqtt.simple import MQTTClient, MQTTException
 
-app_version: str = "1.4.6"
+app_version: str = "1.4.7"
 
 # ==========================================
 # 1. CONFIGURATION
