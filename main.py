0d4aaf11f589525dce91b461d0b59f36c843afba
Greg Cope <gregcope@gmail.com>
2026-09-21 20:15:16 +0100

    1.6.1 OTA test

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ try:
 except ImportError:
     from umqtt.simple import MQTTException, MQTTClient
 
-app_version: str = "1.6.0"
+app_version: str = "1.6.1"
 
 # ==========================================
 # 1. CONFIGURATION
