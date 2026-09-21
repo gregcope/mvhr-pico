cf433dbb5dd3b6853f83bba1a4146e4a4c5427ef
Greg Cope <gregcope@gmail.com>
2026-09-21 16:44:14 +0100

    Update test to 1.4.3 via HA

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ try:
 except ImportError:
     from umqtt.simple import MQTTClient, MQTTException
 
-app_version: str = "1.4.2"
+app_version: str = "1.4.3"
 
 # ==========================================
 # 1. CONFIGURATION
